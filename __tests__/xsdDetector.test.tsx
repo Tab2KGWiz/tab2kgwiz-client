b4cf92a1597a15067769de501b5be7b902a5d5a5
ZcR-01 <zihan1048@live.com>
2024-02-20 11:12:56 +0100

    feat: test decimal values & part of date values

--- __tests__/xsdDetector.test.tsx
+++ __tests__/xsdDetector.test.tsx
@@ -1,12 +1,9 @@
 import "@testing-library/jest-dom";
 import { detectXSD } from "@/app/lib/XSDDetector";
 import { checkDecimalOrInteger } from "@/app/lib/checkDecimalOrInteger";
+import { checkDayMonthYear } from "@/app/lib/checkDayMonthYear";
 
 describe("detect xsd", () => {
-  test("detect xsd", () => {
-    expect(detectXSD("1")).toBe("integer");
-  });
-
   test("Test Date Data Type", () => {
     expect(detectXSD("2002-09-24Z")).toBe("date");
     expect(detectXSD("2002-09-24-06:00")).toBe("date");
@@ -44,9 +41,57 @@ describe("detect xsd", () => {
     expect(detectXSD("5Y2M10D")).not.toBe("duration");
   });
 
-  const values = [
+  test("Test gMonthDay Data Type", () => {
+    expect(detectXSD("12-31")).toBe("gMonthDay");
+    expect(detectXSD("13-31")).not.toBe("gMonthDay");
+    expect(detectXSD("09-32")).not.toBe("gMonthDay");
+  });
+
+  test("Test gYearMonth Data Type", () => {
+    expect(detectXSD("2000-07")).toBe("gYearMonth");
+    expect(detectXSD("1999-13")).not.toBe("gYearMonth");
+  });
+
+  const days = ["22", "31", "01"];
+  const incorrectgDays = ["22", "32", "0"];
+
+  test("Test gDay Data Type", () => {
+    expect(checkDayMonthYear(days)).toBe("gDay");
+    expect(checkDayMonthYear(incorrectgDays)).not.toBe("gDay");
+  });
+
+  const months = ["01", "12", "08"];
+  const incorrectMonths = ["12", "13", "03"];
+
+  test("Test gMonth Data Type", () => {
+    expect(checkDayMonthYear(months)).toBe("gMonth");
+    expect(checkDayMonthYear(incorrectMonths)).not.toBe("gMonth");
+  });
+
+  const years = ["1900", "2003", "1723"];
+
+  test("Test gYear Data Type", () => {
+    expect(checkDayMonthYear(years)).toBe("gYear");
+  });
+
+  test("Test integer Data Type", () => {
+    expect(detectXSD("999")).toBe("integer");
+    expect(detectXSD("+999")).toBe("integer");
+    expect(detectXSD("-999")).toBe("integer");
+    expect(detectXSD("0")).toBe("integer");
+    expect(detectXSD("0.0")).not.toBe("integer");
+  });
+
+  test("Test decimal Data Type", () => {
+    expect(detectXSD("999.50")).toBe("decimal");
+    expect(detectXSD("+999.5450")).toBe("decimal");
+    expect(detectXSD("-999.5230")).toBe("decimal");
+    expect(detectXSD("999.50.2")).not.toBe("decimal");
+  });
+
+  const commaValues = [
     "2,000",
-    "2.233",
+    "2,233",
     "32,121",
     "32,121,000",
     "212,141",
@@ -57,7 +102,35 @@ describe("detect xsd", () => {
     "932,121",
   ];
 
-  test("Test decimal Data Type", () => {
-    expect(checkDecimalOrInteger(values)).toBe("integer");
+  const dotValues = [
+    "2.000",
+    "2.233",
+    "32.121",
+    "32.121.000",
+    "212.141",
+    "311.212.121",
+    "311.181",
+    "832.120",
+    "82.129",
+    "932.121",
+  ];
+
+  const commaDotValues = [
+    "311.212.121",
+    "311,212.121",
+    "311,212.121",
+    "311.212.121",
+    "311.212.121",
+    "311.212.121",
+    "311.181",
+    "832.120",
+    "82.129",
+    "932.121",
+  ];
+
+  test("Test thousand decimal or integer Data Type", () => {
+    expect(checkDecimalOrInteger(commaValues)).toBe("integer");
+    expect(checkDecimalOrInteger(dotValues)).toBe("integer");
+    expect(checkDecimalOrInteger(commaDotValues)).toBe("decimal");
   });
 });
--- src/app/lib/XSDDetector.tsx
+++ src/app/lib/XSDDetector.tsx
@@ -22,9 +22,17 @@ export function detectXSD(value: string) {
       type: "duration",
       regex: /^P(?:\d+Y)?(?:\d+M)?(?:\d+D)?(?:T(?:\d+H)?(?:\d+M)?)?|-P\d+D$/,
     },
+    {
+      type: "gMonthDay",
+      regex: /^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
+    },
+    {
+      type: "gYearMonth",
+      regex: /^\d{4}-(0[1-9]|1[0-2])$/,
+    },
     { type: "boolean", regex: /^(true|false)$/ },
     { type: "integer", regex: /^([+-]?[1-9]\d*|0)$/ }, // 0 valid, 023 invalid
-    { type: "decimal", regex: /^\d*\.?\d*$/ },
+    { type: "decimal", regex: /^[-+]?(\d*\.?\d+|\d+\.\d*)$/ },
     { type: "float", regex: /^^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/ },
     { type: "double", regex: /^-?\d+(\.\d+)?([eE][-+]?\d+)?$/ },
     // { type: "decimal", regex: /^-?\d+(\.\d+)?$/ },
