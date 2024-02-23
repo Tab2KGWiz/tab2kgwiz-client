8ab89ebdaadaa047f86057dc17de70dc176b04bf
ZcR-01 <zihan1048@live.com>
2024-02-23 09:37:55 +0100

    feat: change measurement button style

--- src/app/ui/file-input/measure-form-ui.tsx
+++ src/app/ui/file-input/measure-form-ui.tsx
@@ -14,7 +14,7 @@ const MeasureFormUI: React.FC<Props> = (props): JSX.Element => {
   return (
     <div>
       <button
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 
+        className="block text-white bg-blue-700 hover:bg-blue-800 ml-5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 
         mb-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
         onClick={props.toogleModal}
