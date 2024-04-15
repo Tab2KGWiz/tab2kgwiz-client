bcc530c389ad0a8e854428ce2b3476c46cee5c49
ZcR-01 <zihan1048@live.com>
2024-04-15 10:08:00 +0200

    feat: simple login test

---
+++ src/app/login/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    // const getter = await fetch("http://localhost:8080/");
+
+    // if (getter.ok) {
+    //   alert("Getted!");
+    // } else {
+    //   alert("Get failed!");
+    // }
+
+    const jsonData = {
+      password: password,
+      email: email,
+      username: email,
+    };
+
+    const response = await fetch("http://localhost:8080/suppliers", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(jsonData),
+    });
+
+    if (response.ok) {
+      router.push("");
+    } else {
+      alert("Login failed!");
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
