6936b841d6ba2d7a6c4cfca5e494bc6f7fe4dd09
ZcR-01 <zihan1048@live.com>
2024-04-18 13:16:57 +0200

    feat: post mapping

--- src/app/services/create-mapping.tsx
+++ src/app/services/create-mapping.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const CreateMapping = {
-  createMapping: async (mappingData: {
-    fileName: string;
-    providedBy: string;
-  }): Promise<void> => {
+  createMapping: async (mappingData: { title: string }): Promise<void> => {
     try {
+      // const response = await axios.get("http://localhost:8080/userLoggedIn");
+      axios.defaults.headers.common["Authorization"] =
+        `Bearer ${localStorage.getItem("token")}`;
       const response = await axios.post(
         "http://localhost:8080/mappings",
         mappingData,
