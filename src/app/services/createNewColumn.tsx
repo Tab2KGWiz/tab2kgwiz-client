c22293dfeeaa1f7d293290db200210bada30a21a
ZcR-01 <zihan1048@live.com>
2024-05-09 09:22:08 +0200

    feat: simplify create column

--- src/app/services/createNewColumn.tsx
+++ src/app/services/createNewColumn.tsx
@@ -1,13 +1,23 @@
-"use client";
-import { postColumn } from "./post-column";
+import axios from "axios";
+import Cookies from "js-cookie";
 
-export async function createNewColumn(columnData: {
-  title: string;
-  dataType: string;
-  ontologyType: string;
-}): Promise<number> {
+export async function createNewColumn(
+  mappingId: number,
+  columnData: {
+    title: string;
+    dataType: string;
+    ontologyType: string;
+  },
+): Promise<number> {
   try {
-    if ((await postColumn(columnData)) === 0) {
+    axios.defaults.headers.common["Authorization"] =
+      `Bearer ${Cookies.get("accessToken")}`;
+    const response = await axios.post(
+      `http://localhost:8080/mappings/${mappingId}/columns`,
+      columnData,
+    );
+
+    if (response.status === 200) {
       return 0;
     } else return -1;
   } catch (error) {
