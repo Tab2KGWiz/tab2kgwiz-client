fe8c555d8d0d5ef84953cfe4cc90c5858e1df20f
ZcR-01 <zihan1048@live.com>
2024-05-13 12:53:16 +0200

    feat: create a file context provider to handle as global state

---
+++ src/app/components/file-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+
+type FileContextActions = {
+  file: File | null;
+  setFile: React.Dispatch<React.SetStateAction<File | null>>;
+};
+
+interface FileProviderProps {
+  children: React.ReactNode;
+}
+
+const FileContext = createContext({} as FileContextActions);
+
+export const FileProvider: React.FC<FileProviderProps> = ({ children }) => {
+  const [file, setFile] = useState<File | null>(null);
+
+  return (
+    <FileContext.Provider value={{ file, setFile }}>
+      {children}
+    </FileContext.Provider>
+  );
+};
+
+export const useFile = () => useContext(FileContext);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SnackBarProvider } from "./components/snackbar-provider";
+import { FileProvider } from "./components/file-provider";
+import AppLayout from "./components/Layout/app-layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SnackBarProvider>
-        <body className={inter.className}>{children}</body>
+        <FileProvider>
+          <body className={`${inter.className} fixed inset-0 bg-gray-100`}>
+            {children}
+          </body>
+        </FileProvider>
       </SnackBarProvider>
     </html>
   );
