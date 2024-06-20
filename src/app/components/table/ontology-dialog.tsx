f4b9c60c5908e0cbaf34c354595fde042b0e7bfc
ZcR-01 <zihan1048@live.com>
2024-06-20 10:28:11 +0200

    feat: ontology form dynamic fields

--- src/app/components/table/ontology-dialog.tsx
+++ src/app/components/table/ontology-dialog.tsx
@@ -8,15 +8,25 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
 import Autocomplete from "@mui/material/Autocomplete";
-import { Stack, TextField } from "@mui/material";
+import {
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  Stack,
+  TextField,
+  Typography,
+  Grid,
+} from "@mui/material";
 import useSWR from "swr";
 import axios from "axios";
 import { useEffect } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import { useSnackBar } from "../snackbar-provider";
+import Checkbox from "@mui/material/Checkbox";
 
 interface Props {
   setOntologySelected: React.Dispatch<React.SetStateAction<string>>;
+  headerMapping: Map<string, string>;
 }
 
 interface Prefix {
@@ -39,6 +49,13 @@ const OntologyDialog: React.FC<Props> = (props): JSX.Element => {
   const [prefixRecommendationData, setPrefixRecommendationData] =
     React.useState<[]>([]);
 
+  const [columns, setColumns] = React.useState<string[]>(
+    Array.from(props.headerMapping.keys()),
+  );
+  const [measurementColumns, setMeasurementColumns] = React.useState<string[]>(
+    [],
+  );
+
   const [selectedOntology, setSelectedOntology] = React.useState<string>("");
 
   const { showSnackBar } = useSnackBar();
@@ -51,7 +68,6 @@ const OntologyDialog: React.FC<Props> = (props): JSX.Element => {
 
   const handleSave = () => {
     setOpen(false);
-    console.log(selectedOntology);
     props.setOntologySelected(selectedOntology);
   };
 
@@ -102,13 +118,14 @@ const OntologyDialog: React.FC<Props> = (props): JSX.Element => {
         keepMounted
         onClose={handleDiscard}
         fullScreen
+        scroll="paper"
         aria-describedby="alert-dialog-slide-description"
       >
         <DialogTitle>{"Ontology mapping form"}</DialogTitle>
         <DialogContent>
           <Stack spacing={2}>
             <DialogContentText id="alert-dialog-slide-description">
-              Select the prefix for the ontology
+              Feature of interest
             </DialogContentText>
             <Stack direction={"row"} spacing={3}>
               <Autocomplete
@@ -138,19 +155,179 @@ const OntologyDialog: React.FC<Props> = (props): JSX.Element => {
                 Send
               </Button>
             </Stack>
-            <Autocomplete
-              freeSolo
-              id="combo-box-demo"
-              disableClearable
-              options={prefixRecommendationData}
-              sx={{ width: 300, marginTop: 2 }}
-              getOptionLabel={(option) => option}
-              onSelect={handleOntologySelect}
-              disabled={prefixRecommendationData.length === 0}
-              renderInput={(params) => (
-                <TextField {...params} label="Ontology recommendation" />
-              )}
-            />
+
+            <Stack direction={"row"} spacing={2}>
+              <Autocomplete
+                freeSolo
+                id="combo-box-demo"
+                disableClearable
+                options={prefixRecommendationData}
+                sx={{ width: 300, marginTop: 2 }}
+                getOptionLabel={(option) => option}
+                onSelect={handleOntologySelect}
+                disabled={prefixRecommendationData.length === 0}
+                renderInput={(params) => (
+                  <TextField {...params} label="Ontology recommendation" />
+                )}
+              />
+
+              <Autocomplete
+                id="AutoComplete2"
+                options={columns}
+                sx={{ width: 300, marginTop: 2 }}
+                getOptionLabel={(option) => option}
+                onSelect={handlePrefixSelect}
+                renderInput={(params) => (
+                  <TextField {...params} label="Select a main column" />
+                )}
+              />
+            </Stack>
+
+            <Typography variant="h6">Measurement Columns</Typography>
+            <Stack direction={"row"} spacing={2}>
+              <FormGroup>
+                {Array.from(props.headerMapping.entries()).map(
+                  ([key, value]) => (
+                    <FormControlLabel
+                      key={key}
+                      control={<Checkbox />}
+                      label={key}
+                      onChange={(
+                        event: React.SyntheticEvent,
+                        checked: boolean,
+                      ) => {
+                        if (checked) {
+                          setMeasurementColumns((prev) => [...prev, key]);
+                        } else {
+                          setMeasurementColumns((prev) =>
+                            prev.filter((col) => col !== key),
+                          );
+                        }
+                      }}
+                    />
+                  ),
+                )}
+              </FormGroup>
+            </Stack>
+
+            {measurementColumns.length > 0 && (
+              <Typography variant="h6">Selected Measurement Columns</Typography>
+            )}
+            {measurementColumns.map((column) => (
+              <Stack key={column} spacing={2}>
+                <Typography
+                  variant="body1"
+                  fontWeight={"bold"}
+                >{`${column} - Subject`}</Typography>
+                <Stack direction={"row"} spacing={3}>
+                  <Autocomplete
+                    id="combo-box-demo"
+                    options={prefixesData}
+                    sx={{ width: 300, marginTop: 2 }}
+                    getOptionLabel={(option) => option.prefix}
+                    onSelect={handlePrefixSelect}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Ontology Prefix" />
+                    )}
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="What are you measuring?"
+                    variant="outlined"
+                    onChange={handlePrefixMeasurement}
+                  />
+
+                  <Button
+                    variant="contained"
+                    endIcon={<SendIcon />}
+                    className="bg-blue-600"
+                    disabled={prefixMeasurement === "" || prefixSelected === ""}
+                    onClick={handlePrefixSend}
+                  >
+                    Send
+                  </Button>
+                </Stack>
+
+                <Autocomplete
+                  freeSolo
+                  id="combo-box-demo"
+                  disableClearable
+                  options={prefixRecommendationData}
+                  sx={{ width: 300, marginTop: 2 }}
+                  getOptionLabel={(option) => option}
+                  onSelect={handleOntologySelect}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Ontology recommendation" />
+                  )}
+                />
+                <Typography variant="body2">Predicate-Object</Typography>
+                <Stack direction={"row"} spacing={3}>
+                  <Autocomplete
+                    freeSolo
+                    id="combo-box-demo"
+                    disableClearable
+                    options={prefixRecommendationData}
+                    sx={{ width: 300 }}
+                    getOptionLabel={(option) => option}
+                    //onSelect={handleOntologySelect}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Relates to property" />
+                    )}
+                  />
+                  <Autocomplete
+                    freeSolo
+                    id="combo-box-demo"
+                    disableClearable
+                    options={prefixRecommendationData}
+                    sx={{ width: 300 }}
+                    getOptionLabel={(option) => option}
+                    //onSelect={handleOntologySelect}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Has value" />
+                    )}
+                  />
+                </Stack>
+                <Stack direction={"row"} spacing={3}>
+                  <Autocomplete
+                    freeSolo
+                    id="combo-box-demo"
+                    disableClearable
+                    options={prefixRecommendationData}
+                    sx={{ width: 300 }}
+                    getOptionLabel={(option) => option}
+                    //onSelect={handleOntologySelect}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Has unit" />
+                    )}
+                  />
+                  <Autocomplete
+                    freeSolo
+                    id="combo-box-demo"
+                    disableClearable
+                    options={prefixRecommendationData}
+                    sx={{ width: 300 }}
+                    getOptionLabel={(option) => option}
+                    //onSelect={handleOntologySelect}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Has timestamp" />
+                    )}
+                  />
+                </Stack>
+
+                <Autocomplete
+                  freeSolo
+                  id="combo-box-demo"
+                  disableClearable
+                  options={prefixRecommendationData}
+                  sx={{ width: 300 }}
+                  getOptionLabel={(option) => option}
+                  //onSelect={handleOntologySelect}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Measurement made by" />
+                  )}
+                />
+              </Stack>
+            ))}
           </Stack>
         </DialogContent>
         <DialogActions>
