497ecd6bf954be1fe21a8f47bcd01740113923e6
ZcR-01 <zihan1048@live.com>
2024-07-12 14:54:46 +0200

    feat: signup change to mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { SnackBarProvider } from "./components/snackbar-provider";
 import { FileProvider } from "./components/file-provider";
 import { ThemeProvider } from "@mui/material/styles";
 import { CustomTheme } from "./theme";
+import { CssBaseline } from "@mui/material";
 import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,9 +13,10 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <ThemeProvider theme={CustomTheme}>
+        <CssBaseline />
         <SnackBarProvider>
           <FileProvider>
-            <body className={`${inter.className} fixed inset-0 bg-gray-100`}>
+            <body className={`${inter.className} fixed inset-0`}>
               {children}
             </body>
           </FileProvider>
