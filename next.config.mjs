adaeb4df64dc6d39948adac488f5a4c3574d885a
ZcR-01 <zihan1048@live.com>
2024-10-31 12:18:40 +0100

    feat: mui icon locally

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: false
+  modularizeImports: {
+    "react-icons/?(((\\w*)?/?)*)": {
+      transform: "@react-icons/all-files/{{ matches.[1] }}/{{ member }}",
+      skipDefaultConversion: true,
+    },
+  },
 };
 
 export default nextConfig;
